f6e7d84a82d43d5ed343749f6cc2ef66732e8035
prabhat kasera <pkasera@netpace.com>
2020-08-21 17:03:25 +0530

    Added Automatic index thml generator
    :)

--- script.sh
+++ script.sh
@@ -1,4 +1,17 @@
-<!doctype html>
+#!/bin/bash
+
+cp VZ\ Messages.ipa VZ\ Messages.zip
+unzip VZ\ Messages.zip
+var_path=$PWD
+echo $var_path
+build_ver=$(defaults read $var_path/Payload/VZ\ Messages.app/Info CFBundleShortVersionString)
+build_number=$(defaults read $var_path/Payload/VZ\ Messages.app/Info CFBundleVersion)
+build_detail=$build_ver"("$build_number")"
+
+echo $build_detail
+menifestFilePath="'itms-services://?action=download-manifest&url=https://prabhatliri.github.io/manifest.plist'"
+file="index.html"
+echo '<!doctype html>
 <html lang="en">
 <head>
     <!-- Required meta tags -->
@@ -8,7 +21,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Rebrand Build 3.3.1(16541) </title>
+    <title>Rebrand Build' $build_detail '</title>
   </head> 
   <body>
 <div class="col-xs-1" align="center">
@@ -18,11 +31,11 @@
         <img src="msgplus.png" width="72" height="72" alt="" >
         <br>
         <div class="col-xs-1" align="center">
-        <button type="button" class="btn btn-success" onclick="window.location.href = 'itms-services://?action=download-manifest&url=https://prabhatliri.github.io/manifest.plist';">Download 3.3.1(16541)</button>
+        <button type="button" class="btn btn-success" onclick="window.location.href = '$menifestFilePath';">Download' $build_detail'</button>
       </div>
     <div class="card-body" align="left">
       <h5 class="card-title">Rebrand Build </h5>
-      <h6 class="card-subtitle mb-2 text-muted">Version 3.3.1(16541) </h6>
+      <h6 class="card-subtitle mb-2 text-muted">Version' $build_detail '</h6>
       <p class="card-text"> Release notes on rebrand build :</p>
       <br>
     </div>
@@ -37,4 +50,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
-</html>
+</html>' > $file
+
+rm VZ\ Messages.zip
+rm -r $var_path/Payload
